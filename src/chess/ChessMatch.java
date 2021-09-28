5562815d15dbc81b973c05be64e2685c75dcfeb6
pdcielo <breck_pb@hotmail.com>
2021-09-28 02:15:07 -0300

    Colocando os movimentos do Peão.

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -10,6 +10,7 @@ import boardgame.Position;
 import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Knight;
+import chess.pieces.Pawn;
 import chess.pieces.Queen;
 import chess.pieces.Rook;
 
@@ -191,6 +192,14 @@ public class ChessMatch {
 	}
 	
 	private void initialSetup() {
+		placeNewPiece('a', 2, new Pawn(board, Color.BLUE));
+		placeNewPiece('b', 2, new Pawn(board, Color.BLUE));
+		placeNewPiece('c', 2, new Pawn(board, Color.BLUE));
+		placeNewPiece('d', 2, new Pawn(board, Color.BLUE));
+		placeNewPiece('e', 2, new Pawn(board, Color.BLUE));
+		placeNewPiece('f', 2, new Pawn(board, Color.BLUE));
+		placeNewPiece('g', 2, new Pawn(board, Color.BLUE));
+		placeNewPiece('h', 2, new Pawn(board, Color.BLUE));
 		placeNewPiece('a', 1, new Rook(board, Color.BLUE));
 		placeNewPiece('h', 1, new Rook(board, Color.BLUE));
 		placeNewPiece('b', 1, new Knight(board, Color.BLUE));
@@ -200,6 +209,14 @@ public class ChessMatch {
 		placeNewPiece('d', 1, new Queen(board, Color.BLUE));
 		placeNewPiece('e', 1, new King(board, Color.BLUE));
 		
+		placeNewPiece('a', 7, new Pawn(board, Color.RED));
+		placeNewPiece('b', 7, new Pawn(board, Color.RED));
+		placeNewPiece('c', 7, new Pawn(board, Color.RED));
+		placeNewPiece('d', 7, new Pawn(board, Color.RED));
+		placeNewPiece('e', 7, new Pawn(board, Color.RED));
+		placeNewPiece('f', 7, new Pawn(board, Color.RED));
+		placeNewPiece('g', 7, new Pawn(board, Color.RED));
+		placeNewPiece('h', 7, new Pawn(board, Color.RED));
 		placeNewPiece('a', 8, new Rook(board, Color.RED));
 		placeNewPiece('h', 8, new Rook(board, Color.RED));
 		placeNewPiece('b', 8, new Knight(board, Color.RED));
